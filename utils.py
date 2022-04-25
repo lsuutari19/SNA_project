6bb8aa77844e949f775d476fedae505cc986c7b1
Mikkeep <mikke.pennanen@gmail.com>
2022-04-25 17:05:15 +0300

    Added utils.py to contain needed helper functions for NetworkHandler class. These include file save, file init and Graph generation

---
+++ utils.py
@@ -0,0 +1,37 @@
+"""
+Utility methods for form_network.py
+Generating graph content
+Writing results files
+Initing results files
+"""
+
+from constants import RESULT_PREFIX
+
+
+def generate_graph(graph, datasheet):
+    """
+    columns are 6 and 10, 14 for actor2 and actor1 and actor 3 respectively
+    """
+    for row in range(1, datasheet.nrows):
+        data = datasheet.row_slice(row)
+        actor1 = data[6].value
+        actor2 = data[10].value
+        actor3 = data[14].value
+        graph.add_edges_from([(actor1, actor2), (actor1, actor3)])
+        graph.add_edges_from([(actor2, actor3)])
+
+
+def write_result(output_file, content):
+    """
+    Writes the outcome of processing into a file
+    """
+    with open(RESULT_PREFIX + output_file, "a") as file_out:
+        file_out.write(content)
+
+
+def init_result(inited_file, content):
+    """
+    Empties the result files
+    """
+    with open(RESULT_PREFIX + inited_file, "w") as file_init:
+        file_init.write(content)

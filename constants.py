008ee5d1f0523ba3c14c9579dde8d860fecc5956
Mikkeep <mikke.pennanen@gmail.com>
2022-04-25 17:06:17 +0300

    Inserted all class variables to constants folder for better management

--- constants.py
+++ constants.py
@@ -1,2 +1,9 @@
-file = "./databases/NEC.xls"
-result_prefix = "results/"
+"""
+Constants for form_network.py
+Filelocations and result list
+for appending data
+"""
+
+FILE = "./databases/NEC.xls"
+RESULT_PREFIX = "results/"
+RESULTS = []

2a913f0f7d19467791337ad69c03aa8aae829ca1
Mikkeep <mikke.pennanen@gmail.com>
2022-04-26 10:03:04 +0300

    Added constants for graphs filenames to reduce the hard coded names. Now all the result names come from variables.

--- src/constants.py
+++ src/constants.py
@@ -4,8 +4,11 @@ Filelocations and result list
 for appending data
 """
 
+# Database file
 FILE = "../databases/NEC.xls"
+# Results folder prefix for saving results
 RESULT_PREFIX = "../results/"
+# For saving the lines to write to results folder files
 RESULTS = []
 
 # Datafiles
@@ -13,3 +16,8 @@ BETWEENNES_FILE = "betw_centralities.txt"
 DEGREE_FILE = "degree_centralities.txt"
 EIGEN_FILE = "eigveg_centralities.txt"
 RESULTS_FILE = "result.txt"
+
+# Data distribution graphs
+DEG_DISTR = "degree_distribution.png"
+EIG_DISTR = "eigvec_distribution.png"
+BETW_DISTR = "betw_distribution.png"
